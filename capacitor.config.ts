01acf35938ba191bb87f58deddbcf7519392d6f7
Andreii <abardakov83@gmail.com>
2026-09-22 19:37:15 +0300

    Add Capacitor Android configuration

---
+++ capacitor.config.ts
@@ -0,0 +1,18 @@
+﻿import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.talkhero.app',
+  appName: 'TalkHero',
+  webDir: 'public',
+
+  server: {
+      url: 'https://talk-hero.com/dashboard',
+    cleartext: false,
+  },
+
+  android: {
+    allowMixedContent: false,
+  },
+};
+
+export default config;
