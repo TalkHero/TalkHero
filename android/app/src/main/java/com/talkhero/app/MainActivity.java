c6541bc80d8850c2e1d8d2affa3a8e393ad85d14
Andreii <abardakov83@gmail.com>
2026-09-22 19:31:34 +0300

    Add Android app and handle system bar insets

---
+++ android/app/src/main/java/com/talkhero/app/MainActivity.java
@@ -0,0 +1,70 @@
+package com.talkhero.app;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.PermissionRequest;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView webView = getBridge().getWebView();
+        View rootView = webView.getRootView();
+
+        ViewCompat.setOnApplyWindowInsetsListener(rootView, (view, windowInsets) -> {
+            Insets systemBars = windowInsets.getInsets(
+                    WindowInsetsCompat.Type.systemBars()
+            );
+
+            view.setPadding(
+                    systemBars.left,
+                    systemBars.top,
+                    systemBars.right,
+                    systemBars.bottom
+            );
+
+            return WindowInsetsCompat.CONSUMED;
+        });
+
+        ViewCompat.requestApplyInsets(rootView);
+
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onPermissionRequest(PermissionRequest request) {
+                runOnUiThread(() -> {
+                    if (ContextCompat.checkSelfPermission(
+                            MainActivity.this,
+                            Manifest.permission.RECORD_AUDIO
+                    ) != PackageManager.PERMISSION_GRANTED) {
+                        request.deny();
+                        return;
+                    }
+
+                    for (String resource : request.getResources()) {
+                        if (PermissionRequest.RESOURCE_AUDIO_CAPTURE.equals(resource)) {
+                            request.grant(new String[]{
+                                    PermissionRequest.RESOURCE_AUDIO_CAPTURE
+                            });
+                            return;
+                        }
+                    }
+
+                    request.deny();
+                });
+            }
+        });
+    }
+}
